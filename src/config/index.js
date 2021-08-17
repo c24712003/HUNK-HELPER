fe74e1c6db8610cc52d84508c95efca29df9dc44
Zhou Jin An <c24712003@gmail.com>
2021-08-17 14:02:23 +0800

    commit

--- src/config/index.js
+++ src/config/index.js
@@ -5,9 +5,9 @@ exports.default = {
     line_bot: {
         channelId: 1656308867,
         channelSecret: 'a7374fc2a3252312886268032680146f',
-        channelAccessToken: 'fp22kjPmSL51wdKGM60jsdgIC2e78eYp9vF37dFZazP9mCN866cXOkG',
+        channelAccessToken: 'fp22kjPmSL51wdKGM60jsdgIC2e78eYp9vF37dFZazP9mCN866cXOkG+Aij/S8QzGx5s59snEEEnSVSnzhVTMUf5tUPERjozbwoN1m8x70DaOWnhU9mDdwqYwNc521kIuWtLO0XHQpcq1vKLnpRrWQdB04t89/1O/w1cDnyilFU=',
         pushMessageUserId: 'U202121644583b43b4c87b6cdb09b0dad',
-        richMenuId: 'richmenu-2f725826e55577b536b72ad51da44566'
+        richMenuId: 'richmenu-3946aebee6ccbb6210854da44330a15d'
     },
     movie_tmdb: {
         apiKey: 'b09708e1599c86aac2d007b9bfb0f771'
